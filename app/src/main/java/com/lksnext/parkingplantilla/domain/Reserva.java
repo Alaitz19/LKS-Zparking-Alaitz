512dcf63eb3ac33e098c836e904e225352cc7756
Alaitz <alaitzshan19@gmail.com>
2025-06-08 11:53:37 +0200

    style: añadir layout de mi reservations

--- app/src/main/java/com/lksnext/parkingplantilla/domain/Reserva.java
+++ app/src/main/java/com/lksnext/parkingplantilla/domain/Reserva.java
@@ -67,4 +67,32 @@ public class Reserva {
     public void setId(String id) {
         this.id = id;
     }
+
+    @Override
+    public String toString() {
+        return "Reserva{" +
+                "fecha='" + fecha + '\'' +
+                ", usuario='" + usuario + '\'' +
+                ", id='" + id + '\'' +
+                ", plaza=" + plaza +
+                ", hora=" + hora +
+                '}';
+    }
+    public int remainingTime() {
+        if (getHoraInicio() != null && getHoraFin() != null) {
+            long currentTime = System.currentTimeMillis();
+            long startTime = getHoraInicio().getHoraInicio();
+            long endTime = getHoraFin().getHoraFin();
+
+            if (currentTime < startTime) {
+                return (int) ((startTime - currentTime) / 60000); // tiempo restante en minutos
+            } else if (currentTime <= endTime) {
+                return (int) ((endTime - currentTime) / 60000); // tiempo restante en minutos
+            }
+        } else {
+            System.out.println("Hora de inicio o fin no establecida.");
+        }
+        return 0; // Si no hay tiempo restante, devolvemos 0
+    }
+
 }
